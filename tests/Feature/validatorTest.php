d52fb05cece8d90859523fe1b2c78753611b3da2
Sofa <muhammadsofa52@yahoo.com>
2023-08-08 16:41:18 +0700

    Validation Exception

--- tests/Feature/validatorTest.php
+++ tests/Feature/validatorTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class validatorTest extends TestCase
@@ -52,6 +53,32 @@ class validatorTest extends TestCase
         $message = $validator->getMessageBag();
 
         Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    // Validation Exception
+    public function testValidatorValidationException()
+    {
+        $data = [
+            "username" => "",
+            "password" => ""
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required"
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        try {
+            $validator->validate();
+            self::fail("validationException not thrown");
+        }catch (ValidationException $exception){
+            self::assertNotNull($exception->validator);
+            $message = $exception->validator->errors();
+            Log::error($message->toJson(JSON_PRETTY_PRINT));
+        }
     }
     
 }
